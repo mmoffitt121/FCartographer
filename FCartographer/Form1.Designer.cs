f96b564f8a2457d83aa5e19ec1cfcbb88858a74c
mmoffitt121 <70288684+mmoffitt121@users.noreply.github.com>
2021-10-07 15:52:54 -0500

    Add project files.

---
+++ FCartographer/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿
+namespace FCartographer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Canvas = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ElevationSettings = new System.Windows.Forms.Panel();
+            this.HeightLabel = new System.Windows.Forms.Label();
+            this.HeightControl = new System.Windows.Forms.TrackBar();
+            this.SizeLabel = new System.Windows.Forms.Label();
+            this.SizeControl = new System.Windows.Forms.TrackBar();
+            this.StrengthLabel = new System.Windows.Forms.Label();
+            this.StrengthControl = new System.Windows.Forms.TrackBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.ElevationSettings.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.HeightControl)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SizeControl)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.StrengthControl)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Canvas
+            // 
+            this.Canvas.BackColor = System.Drawing.Color.White;
+            this.Canvas.Location = new System.Drawing.Point(457, 231);
+            this.Canvas.Name = "Canvas";
+            this.Canvas.Size = new System.Drawing.Size(821, 509);
+            this.Canvas.TabIndex = 0;
+            this.Canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.Canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.Canvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(103, 961);
+            this.panel1.TabIndex = 1;
+            // 
+            // ElevationSettings
+            // 
+            this.ElevationSettings.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.ElevationSettings.Controls.Add(this.HeightLabel);
+            this.ElevationSettings.Controls.Add(this.HeightControl);
+            this.ElevationSettings.Controls.Add(this.SizeLabel);
+            this.ElevationSettings.Controls.Add(this.SizeControl);
+            this.ElevationSettings.Controls.Add(this.StrengthLabel);
+            this.ElevationSettings.Controls.Add(this.StrengthControl);
+            this.ElevationSettings.Controls.Add(this.button1);
+            this.ElevationSettings.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ElevationSettings.Location = new System.Drawing.Point(0, 0);
+            this.ElevationSettings.Name = "ElevationSettings";
+            this.ElevationSettings.Size = new System.Drawing.Size(109, 961);
+            this.ElevationSettings.TabIndex = 0;
+            // 
+            // HeightLabel
+            // 
+            this.HeightLabel.AutoSize = true;
+            this.HeightLabel.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.HeightLabel.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.HeightLabel.ForeColor = System.Drawing.Color.DarkRed;
+            this.HeightLabel.Location = new System.Drawing.Point(10, 241);
+            this.HeightLabel.Name = "HeightLabel";
+            this.HeightLabel.Size = new System.Drawing.Size(45, 17);
+            this.HeightLabel.TabIndex = 6;
+            this.HeightLabel.Text = "Height";
+            // 
+            // HeightControl
+            // 
+            this.HeightControl.Location = new System.Drawing.Point(2, 260);
+            this.HeightControl.Maximum = 255;
+            this.HeightControl.Name = "HeightControl";
+            this.HeightControl.Size = new System.Drawing.Size(104, 45);
+            this.HeightControl.TabIndex = 5;
+            this.HeightControl.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.HeightControl.Value = 100;
+            // 
+            // SizeLabel
+            // 
+            this.SizeLabel.AutoSize = true;
+            this.SizeLabel.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.SizeLabel.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.SizeLabel.ForeColor = System.Drawing.Color.DarkRed;
+            this.SizeLabel.Location = new System.Drawing.Point(11, 189);
+            this.SizeLabel.Name = "SizeLabel";
+            this.SizeLabel.Size = new System.Drawing.Size(31, 17);
+            this.SizeLabel.TabIndex = 4;
+            this.SizeLabel.Text = "Size";
+            // 
+            // SizeControl
+            // 
+            this.SizeControl.Location = new System.Drawing.Point(3, 209);
+            this.SizeControl.Maximum = 255;
+            this.SizeControl.Name = "SizeControl";
+            this.SizeControl.Size = new System.Drawing.Size(104, 45);
+            this.SizeControl.TabIndex = 3;
+            this.SizeControl.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.SizeControl.Value = 100;
+            // 
+            // StrengthLabel
+            // 
+            this.StrengthLabel.AutoSize = true;
+            this.StrengthLabel.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.StrengthLabel.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.StrengthLabel.ForeColor = System.Drawing.Color.DarkRed;
+            this.StrengthLabel.Location = new System.Drawing.Point(11, 138);
+            this.StrengthLabel.Name = "StrengthLabel";
+            this.StrengthLabel.Size = new System.Drawing.Size(56, 17);
+            this.StrengthLabel.TabIndex = 2;
+            this.StrengthLabel.Text = "Strength";
+            // 
+            // StrengthControl
+            // 
+            this.StrengthControl.Location = new System.Drawing.Point(3, 158);
+            this.StrengthControl.Maximum = 255;
+            this.StrengthControl.Name = "StrengthControl";
+            this.StrengthControl.Size = new System.Drawing.Size(104, 45);
+            this.StrengthControl.TabIndex = 1;
+            this.StrengthControl.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.StrengthControl.Value = 100;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(39, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Gainsboro;
+            this.panel2.Controls.Add(this.ElevationSettings);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(1596, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(109, 961);
+            this.panel2.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(1705, 961);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.Canvas);
+            this.Name = "Form1";
+            this.Text = "FCartographer";
+            this.ElevationSettings.ResumeLayout(false);
+            this.ElevationSettings.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.HeightControl)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SizeControl)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.StrengthControl)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Canvas;
+        private System.Windows.Forms.Panel ElevationSettings;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar StrengthControl;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label StrengthLabel;
+        private System.Windows.Forms.Label SizeLabel;
+        private System.Windows.Forms.TrackBar SizeControl;
+        private System.Windows.Forms.Label HeightLabel;
+        private System.Windows.Forms.TrackBar HeightControl;
+    }
+}
+
---
+++ FCartographer/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FCartographer
+{
+    public partial class Form1 : Form
+    {
+        private bool painting;
+
+        // Height Data
+        private Graphics height_data_graphics;
+        private Bitmap height_data;
+
+        // Display to user
+        private Graphics display_graphics;
+        private Bitmap display_data;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            painting = false;
+
+            SolidBrush brush = new SolidBrush(Color.FromArgb(255, 255, 255, 255));
+
+            // Height Editing Initialization
+            height_data = new Bitmap(Canvas.Width, Canvas.Height);
+            height_data_graphics = Graphics.FromImage(height_data);
+            height_data_graphics.FillRectangle(brush, 0, 0, Canvas.Width, Canvas.Height);
+
+            // Height Editing Initialization
+            display_data = new Bitmap(Canvas.Width, Canvas.Height);
+            display_graphics = Canvas.CreateGraphics();
+            display_graphics.FillRectangle(brush, 0, 0, Canvas.Width, Canvas.Height);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            height_data.Save("Output.png", System.Drawing.Imaging.ImageFormat.Png);
+        }
+
+        // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+        // Mouse Control
+        // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            painting = true;
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            painting = false;
+            xprime = null;
+            yprime = null;
+        }
+
+        private int? xprime = null;
+        private int? yprime = null;
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            ElevationBrushTool(sender, e);
+            
+        }
+
+        enum ToolUsed
+        {
+            ElevationBrush,
+            ElevationSmoothenBrush
+        }
+
+        private void ElevationBrushTool(object sender, MouseEventArgs e)
+        {
+            if (painting)
+            {
+                SolidBrush brush = new SolidBrush(Color.FromArgb(StrengthControl.Value, HeightControl.Value, HeightControl.Value, HeightControl.Value));
+                // Pen pen = new Pen(Color.FromArgb(StrengthControl.Value, 32, 32, 32), SizeControl.Value);
+                // graphics.DrawLine(pen, new Point(xprime ?? e.X, yprime ?? e.Y), new Point(e.X, e.Y));
+                height_data_graphics.FillEllipse(brush, e.X - SizeControl.Value / 2, e.Y - SizeControl.Value / 2, SizeControl.Value, SizeControl.Value);
+
+                xprime = e.X;
+                yprime = e.Y;
+
+                FastRender();
+            }
+        }
+
+        private void FastRender()
+        {
+
+
+            // Canvas.DrawToBitmap(bmp, new Rectangle(0, 0, Canvas.Width, Canvas.Height));
+            /*for (int i = 1; i < Canvas.Width-1; i++)
+            {
+                for (int j = 1; j < Canvas.Height-1; j++)
+                {
+                    int shade = bmp.GetPixel(i, j).R;// - bmp.GetPixel(i, j).R;
+                    shade = (shade + 255) / 2;
+                    new_bmp.SetPixel(i, j, Color.FromArgb(255, shade, shade, shade));
+                }
+            }*/
+
+            display_graphics.DrawImage(height_data, 0, 0);
+        }
+    }
+}
