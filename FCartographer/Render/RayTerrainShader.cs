269d129a3fa8da1044ac6db8eafb7be404042b3c
mmoffitt121 <70288684+mmoffitt121@users.noreply.github.com>
2022-02-07 11:15:44 -0600

    Shadows working, buggy

--- FCartographer/Render/RayTerrainShader.cs
+++ FCartographer/Render/RayTerrainShader.cs
@@ -26,6 +26,11 @@ namespace FCartographer
         public float angle;
 
         /// <summary>
+        /// How much the light drops off around corners;
+        /// </summary>
+        public float dropoff;
+
+        /// <summary>
         /// Render override function
         /// </summary>
         public override void Render()
@@ -41,9 +46,12 @@ namespace FCartographer
             int wid = GetData().Width * 4;
             int hei = GetData().Height;
 
-            float dx = MathF.Cos((180 + direction) * MathF.PI / 180) * MathF.Sin((180 + angle) * MathF.PI / 180);
-            float dy = MathF.Sin((180 + direction) * MathF.PI / 180) * MathF.Sin((180 + angle) * MathF.PI / 180);
-            float dh = MathF.Cos((180 + angle) * MathF.PI / 180);
+            float dx = MathF.Cos((180 + direction) * MathF.PI / 180) * MathF.Sin((/*180 + */angle) * MathF.PI / 180);
+            float dy = MathF.Sin((180 + direction) * MathF.PI / 180) * MathF.Sin((/*180 + */angle) * MathF.PI / 180);
+            float dh = MathF.Cos((/*180 + */angle) * MathF.PI / 180);
+
+            dropoff = 0.9f;
+            direction = 180f;
 
             for (int i = 0; i < wid * hei; i += 4)
             {
@@ -51,20 +59,27 @@ namespace FCartographer
                 float y = i / wid;
                 float h = inp[i];
                 float luminosity = 1f;
-                while (x < wid / 4 && x >= 0 && y < hei && y >= 0 && h <= 255 && h >= 0)
+                while (x < wid / 4 && x >= 0 && y < hei && y >= 0 && h <= 255 && h >= 0 && luminosity > 0)
                 {
-
+                    //Debug.WriteLine(h + " " + inp[wid * (int)y + 4 * (int)x]);
+                    if (luminosity > 1 + dropoff * (h - inp[wid * (int)y + 4 * (int)x]))
+                    {
+                        luminosity = 1 + dropoff * (h - inp[wid * (int)y + 4 * (int)x]);
+                    }
 
                     x += dx;
                     y += dy;
                     h += dh;
                 }
+
+                //Debug.WriteLine(luminosity + " --- ");
+
                 // Write to output
 
                 outp[i + 3] = 255;//(byte)(Math.Clamp(dir * magnitude, 0, 255));
-                outp[i + 2] = (byte)Lerper.Lerp(Math.Clamp(128, 0, 255), outp[i + 2], 1 - opacity);
-                outp[i + 1] = (byte)Lerper.Lerp(Math.Clamp(128, 0, 255), outp[i + 1], 1 - opacity);
-                outp[i + 0] = (byte)Lerper.Lerp(Math.Clamp(128, 0, 255), outp[i + 0], 1 - opacity);
+                outp[i + 2] = (byte)Lerper.Lerp(Math.Clamp(255 * luminosity, 0, 255), outp[i + 2], 1 - opacity);
+                outp[i + 1] = (byte)Lerper.Lerp(Math.Clamp(255 * luminosity, 0, 255), outp[i + 1], 1 - opacity);
+                outp[i + 0] = (byte)Lerper.Lerp(Math.Clamp(255 * luminosity, 0, 255), outp[i + 0], 1 - opacity);
             }
 
             BitmapDataConverter.DrawImage(GetOutput(), outp, true);
@@ -80,6 +95,7 @@ namespace FCartographer
             intensity = 30;
             angle = -30f;
             direction = -10f;
+            dropoff = 0.5f;
         }
     }
 }
