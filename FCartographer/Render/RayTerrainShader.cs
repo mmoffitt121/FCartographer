10f5ea1352303d86e9c9eadbc530d1634f33c9f4
mmoffitt121 <70288684+mmoffitt121@users.noreply.github.com>
2022-03-04 02:20:29 -0600

    Test

--- FCartographer/Render/RayTerrainShader.cs
+++ FCartographer/Render/RayTerrainShader.cs
@@ -4,6 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Diagnostics;
+using OpenCL.Net.Extensions;
+using OpenCL.Net;
+using System.Linq;
 
 namespace FCartographer
 {
@@ -55,7 +58,8 @@ namespace FCartographer
         /// </summary>
         public override void Render()
         {
-            RenderShadows();
+            OpenCLRenderShadows();
+            //RenderShadows();
         }
 
         private void RenderShadows()
@@ -75,6 +79,70 @@ namespace FCartographer
             byte lr = lightcolor.R;
             byte lg = lightcolor.G;
             byte lb = lightcolor.B;
+
+            float luminosity;
+            for (int i = 0; i < wid * hei; i += 4)
+            {
+                float x = i % wid / 4;
+                float y = i / wid;
+                float h = inp[i];
+                luminosity = 1f;
+                while (x < wid / 4 && x >= 0 && y < hei && y >= 0 && h <= 255 && h >= 0 && luminosity > 0)
+                {
+                    if (luminosity > 1 + dropoff * (h - bias - inp[wid * (int)y + 4 * (int)x]))
+                    {
+                        luminosity = 1 + dropoff * (h - bias - inp[wid * (int)y + 4 * (int)x]);
+                    }
+
+                    x += dx;
+                    y += dy;
+                    h += dh;
+                }
+
+                outp[i + 3] = 255;
+                outp[i + 2] = (byte)Math.Clamp(amb * outp[i + 2] + luminosity * lr * intensity * ((float)outp[i + 2]) / 255, 0, 255);
+                outp[i + 1] = (byte)Math.Clamp(amb * outp[i + 1] + luminosity * lg * intensity * ((float)outp[i + 1]) / 255, 0, 255);
+                outp[i + 0] = (byte)Math.Clamp(amb * outp[i + 0] + luminosity * lb * intensity * ((float)outp[i + 0]) / 255, 0, 255);
+            }
+
+            BitmapDataConverter.DrawImage(GetOutput(), outp, true);
+        }
+
+        private void OpenCLRenderShadows()
+        {
+            byte[] inp = BitmapDataConverter.BitmapToByteArray(GetData());
+            byte[] outp = BitmapDataConverter.BitmapToByteArray(GetOutput());
+
+            int wid = GetData().Width * 4;
+            int hei = GetData().Height;
+
+            float dx = MathF.Cos((180 - direction) * MathF.PI / 180) * MathF.Sin((angle + 90) * MathF.PI / 180);
+            float dy = MathF.Sin((180 - direction) * MathF.PI / 180) * MathF.Sin((angle + 90) * MathF.PI / 180);
+            float dh = MathF.Cos((angle + 90) * MathF.PI / 180);
+
+            float amb = ((float)ambient) / 255;
+
+            byte lr = lightcolor.R;
+            byte lg = lightcolor.G;
+            byte lb = lightcolor.B;
+
+            Event event0;
+            ErrorCode err;
+            Platform[] platforms = Cl.GetPlatformIDs(out err);
+            Device[] devices = Cl.GetDeviceIDs(platforms[0], DeviceType.Gpu, out err);
+            Device device = devices[0];
+            Context context = Cl.CreateContext(null, 1, devices, null, IntPtr.Zero, out err);
+            CommandQueue cmdQueue = Cl.CreateCommandQueue(context, device, CommandQueueProperties.None, out err);
+
+            // Create and build a program from our OpenCL-C source code !!! BYTE WON'T WORK?? !!!
+            string programSource = @"
+            __kernel void doubleMe(__global byte* input, __global float* output)
+            {
+                size_t i = get_global_id(0);
+                output = input + input;
+            };";
+            OpenCL.Net.Program program = Cl.CreateProgramWithSource(context, 1, new[] { programSource }, null, out err);
+            Cl.BuildProgram(program, 0, null, string.Empty, null, IntPtr.Zero);  //"-cl-mad-enable"
 
             float luminosity;
             for (int i = 0; i < wid * hei; i += 4)
