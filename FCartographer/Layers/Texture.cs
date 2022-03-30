1a0f6944d456205582c04ecb11db99d0682f47d1
mmoffitt121 <mmoffitt121@gmail.com>
2022-03-30 17:50:16 -0500

    Texture class added

--- FCartographer/Layers/Texture.cs
+++ FCartographer/Layers/Texture.cs
@@ -11,11 +11,54 @@ namespace FCartographer
     {
         public string name;
         public int opacity;
+        private Bitmap mask;
         private Bitmap texture;
+
+        private Generator gen;
 
         public Bitmap GetTexture()
         {
             return texture;
+        }
+
+        public void SetTexture(Bitmap _texture)
+        {
+            texture = new Bitmap(_texture);
+        }
+
+        public void SetGenerator(int genmode)
+        {
+            switch (genmode)
+            {
+                case 0:
+                    gen = new DirtTextureGenerator(texture);
+                    break;
+            }
+        }
+
+        public Generator GetGenerator()
+        {
+            return gen;
+        }
+
+        public void GenerateTexture()
+        {
+            if (gen != null)
+            {
+                gen.Generate();
+            }
+        }
+
+        public void InitializeTexture(int genmode, int wid, int hei)
+        {
+            SetTexture(new Bitmap(wid, hei));
+            SetGenerator(genmode);
+            GenerateTexture();
+        }
+
+        public void UpdateTexture()
+        {
+            GenerateTexture();
         }
     }
 }
